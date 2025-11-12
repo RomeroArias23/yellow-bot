c96004964cf4cb31341a6f5e62cd90fa29a78954
RomeroArias23 <137248494+RomeroArias23@users.noreply.github.com>
2025-11-11 19:29:07 -0600

    config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  DISCORD_TOKEN: process.env.DISCORD_TOKEN,   // ✅ match Railway variable name
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN, 
   API_BASE: process.env.API_BASE || 'https://yellow-project-api.onrender.com'
 };
