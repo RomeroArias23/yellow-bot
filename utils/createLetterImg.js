66140f7280d0fe0433417cf582e176189b9cdb76
RomeroArias23 <137248494+RomeroArias23@users.noreply.github.com>
2025-11-19 19:58:33 -0600

    fixing

--- utils/createLetterImg.js
+++ utils/createLetterImg.js
@@ -2,37 +2,52 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const fs = require('fs');
 
+function escapeHtml(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;")
+    .replace(/\n/g, "<br>");
+}
+
 async function createLetterImage(addressee, letter) {
-  // local route to the HTML to be rendered
   const htmlPath = path.join(__dirname, 'template.html');
+
   let html = fs.readFileSync(htmlPath, 'utf8');
 
-  // Insert dynamic content
   html = html
-    .replace('{{LETTER}}', letter)
+    .replace('{{LETTER}}', escapeHtml(letter))
     .replace('{{DATE}}', new Date().toLocaleDateString('es-MX'))
-    .replace('{{ADDRESSEE}}', addressee);
+    .replace('{{ADDRESSEE}}', escapeHtml(addressee));
 
-  // launch Chrome Headless
   const browser = await puppeteer.launch({
-    headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: true,
+    executablePath: puppeteer.executablePath(),  // ← necesario en Railway
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--single-process',
+      '--disable-dev-tools'
+    ]
   });
 
   const page = await browser.newPage();
 
-  // launch html (serverless)
-  await page.setContent(html, { waitUntil: 'networkidle0' });
-
-  // selects the main content
-  const element = await page.$('#card');
-
-  const imageBuffer = await element.screenshot({
-    type: 'png'
+  await page.setContent(html, {
+    waitUntil: ['domcontentloaded', 'networkidle0']
   });
 
+  await page.waitForSelector('#card');
+
+  const element = await page.$('#card');
+  const image = await element.screenshot({ type: 'png' });
+
   await browser.close();
-  return imageBuffer;
+  return image;
 }
 
 module.exports = createLetterImage;
